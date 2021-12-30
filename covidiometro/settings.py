bff6705e59349e67ab9e596443c03ae82c72f28c
Radu Andries <me@admiral0.it>
2021-12-30 01:39:12 +0100

    add misc deps

--- covidiometro/settings.py
+++ covidiometro/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_results',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_TIMEZONE = 'Europe/Rome'
+CELERY_RESULT_BACKEND = 'drango-db'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
